79aad2a9cbebe3f1a6a8c2238fde8b967906acf2
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-22 21:33:46 +0200

    simultaneousGesture

--- FlashzillaInSwiftUI/ContentView.swift
+++ FlashzillaInSwiftUI/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
                 )
                 .padding()
         }
-        .highPriorityGesture(
+        .simultaneousGesture(
             TapGesture()
                 .onEnded({
                     print("VStack tapped")
