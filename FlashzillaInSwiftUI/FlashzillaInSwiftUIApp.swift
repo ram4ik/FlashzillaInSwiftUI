e8e3a66776f6317b24493ea920b01bd2bfaa5329
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-22 21:05:16 +0200

    Initial Commit

---
+++ FlashzillaInSwiftUI/FlashzillaInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaInSwiftUIApp.swift
+//  FlashzillaInSwiftUI
+//
+//  Created by test on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
