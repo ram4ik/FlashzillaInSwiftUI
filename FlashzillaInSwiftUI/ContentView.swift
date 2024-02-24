6c94476e2bd14def3f108b3e0dff54902e00917e
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-24 11:46:15 +0200

    Disabling user interactivity with allowsHitTesting()

--- FlashzillaInSwiftUI/ContentView.swift
+++ FlashzillaInSwiftUI/ContentView.swift
@@ -9,36 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
-    
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    print("Rectangle tapped!")
                 }
-            }
-        
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
+            
+            Circle()
+                .fill(.red)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    print("Circle tapped!")
                 }
-            }
-        
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+                .allowsHitTesting(false)
+        }
     }
 }
 
