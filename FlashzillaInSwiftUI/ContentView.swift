62d43c69e611adec7d387c745f73c27c7594f066
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-24 18:47:27 +0200

    Triggering events repeatedly using a time

--- FlashzillaInSwiftUI/ContentView.swift
+++ FlashzillaInSwiftUI/ContentView.swift
@@ -9,19 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    
+    @State private var counter = 0
+    
     var body: some View {
-        VStack {
-            Text("Hello")
-            
-            Spacer()
-            
-            Text("World")
-        }
-        .contentShape(.rect)
-        .onTapGesture {
-            print("VStack tapped!")
-        }
+        Text("Timer")
+            .onReceive(timer) { time in
+                if counter == 5 {
+                    timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
+                }
+                
+                counter += 1
+            }
     }
+    
 }
 
 #Preview {
