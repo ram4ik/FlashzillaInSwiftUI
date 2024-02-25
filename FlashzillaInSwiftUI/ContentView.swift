b34d972670bfa3fd33f04c738ea8f9b5303e690d
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-25 21:10:40 +0200

    Counting down with a Timer

--- FlashzillaInSwiftUI/ContentView.swift
+++ FlashzillaInSwiftUI/ContentView.swift
@@ -18,11 +18,25 @@ struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
     @State private var cards = Array<Card>(repeating: .example, count: 10)
     
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+    
     var body: some View {
         ZStack {
             LinearGradient(colors: [.gray.opacity(0.6), .white, .gray.opacity(0.7)], startPoint: .bottomLeading, endPoint: .topTrailing)
                 .edgesIgnoringSafeArea(.all)
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -56,6 +70,20 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .padding()
                 }
+            }
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                isActive = true
+            } else {
+                isActive = false
             }
         }
     }
